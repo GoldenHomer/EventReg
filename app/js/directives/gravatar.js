ac7c452439d61f0ed683cc31dffa2a226b825fff
Jose Contreras <jocontre@joselcontreras.com>
2014-10-20 15:50:02 -0500

    gravatar custom directive added

---
+++ app/js/directives/gravatar.js
@@ -0,0 +1,16 @@
+'use strict';
+
+eventApp.directive('gravatar', function (gravatarUrlBuilder) {
+	return {
+		restrict: 'E',
+		template: '<img />',
+		replace: true, // You see that the gravatar tag is replaced with img tag when inspecting
+		link: function (scope, element, attrs, controller) {
+			attrs.$observe('email', function(newValue, oldValue){
+				if (newValue !== oldValue){
+					attrs.$set('src', gravatarUrlBuilder.buildGravatarUrl(newValue));
+				}
+			});
+		}
+	}
+});
